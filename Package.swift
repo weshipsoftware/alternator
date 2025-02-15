4e5fc6b1bf1060a4bc721d2e3023da789732bf21
Jarrod Taylor <email@jarrodtaylor.me>
2025-02-15 08:44:28 -0500

    Formats packages

--- Package.swift
+++ Package.swift
@@ -3,16 +3,14 @@
 import PackageDescription
 
 let package = Package(
-  name      : "alternator",
-  platforms : [.macOS(.v13)],
-
+  name: "alternator",
+  platforms: [.macOS(.v13)],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-argument-parser",     from: "1.5.0"),
-    .package(url: "https://github.com/jarrodtaylor/fs-diff-stream.git", from: "1.0.0"),
-    .package(url: "https://github.com/johnsundell/ink.git",             from: "0.6.0"),
-    .package(url: "git@github.com:simonbs/Prettier.git",                from: "0.1.0")
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
+    .package(url: "https://github.com/jarrodtaylor/fs-diff-stream", from: "1.0.0"),
+    .package(url: "https://github.com/johnsundell/ink",             from: "0.6.0"),
+    .package(url: "https://github.com/simonbs/Prettier",            from: "0.1.0")
   ],
-
   targets: [
     .executableTarget(
       name: "alternator",
@@ -25,4 +23,6 @@ let package = Package(
         .product(name: "PrettierHTML",    package: "Prettier"),
         .product(name: "PrettierPostCSS", package: "Prettier")
       ]
-)])
+    )
+  ]
+)
