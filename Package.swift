b6d59691815acdbb6699b546eece33752bd1219f
Jarrod Taylor <email@jarrodtaylor.me>
2025-05-19 20:00:58 -0400

    Removes unused spm packages

--- Package.swift
+++ Package.swift
@@ -7,27 +7,15 @@ let package = Package(
   platforms: [.macOS(.v13)],
   dependencies: [
     .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
-    .package(url: "https://github.com/jarrodtaylor/fs-diff-stream", from: "1.0.0"),
-    .package(url: "https://github.com/jarrodtaylor/RESTless",       from: "1.0.0"),
-    .package(url: "https://github.com/johnsundell/ink",             from: "0.6.0"),
-    .package(url: "https://github.com/simonbs/Prettier",            from: "0.1.0")
+    .package(url: "https://github.com/johnsundell/ink",             from: "0.6.0")
   ],
   targets: [
     .executableTarget(
       name: "alternator",
       dependencies: [
         .product(name: "ArgumentParser",  package: "swift-argument-parser"),
-        .product(name: "FSDiffStream",    package: "fs-diff-stream"),
-        .product(name: "Ink",             package: "ink"),
-        .product(name: "Prettier",        package: "Prettier"),
-        .product(name: "PrettierBabel",   package: "Prettier"),
-        .product(name: "PrettierHTML",    package: "Prettier"),
-        .product(name: "PrettierPostCSS", package: "Prettier"),
-        .product(name: "RESTless",        package: "RESTless")
-      ],
-      path: ".",
-      exclude: ["docs/", "README.md", "LICENSE.md"],
-      sources: ["Alternator.swift"]
+        .product(name: "Ink",             package: "ink")
+      ]
     )
   ]
 )
